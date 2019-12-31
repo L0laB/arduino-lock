6b6a512cb504f554e59dd7101582265085012a41
l0lab <lola.behrens@gmail.com>
2019-12-31 18:29:22 +0100

    change to blinking led

--- ks5-arduino.ino
+++ ks5-arduino.ino
@@ -7,7 +7,7 @@
  http://www.arduino.cc/en/Tutorial/Knob
 */
 
-#include <Servo.h>
+/*#include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
 
@@ -23,4 +23,33 @@ void loop() {
   val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
   myservo.write(val);                  // sets the servo position according to the scaled value
   delay(15);                           // waits for the servo to get there
+}
+*/
+
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
 }
